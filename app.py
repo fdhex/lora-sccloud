649ad179d8f8902569a57e903a75a7d972558b9c
Florian Dudouet <florian.dudouet@swisscom.com>
2016-06-21 15:40:01 +0200

    lora sccloud

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
 # import dependencies
 import os
+import json
 from flask import Flask
+from flask import request
+from flask.ext.mongoengine import MongoEngine
+import untangle
 
 # bootstrap the app
 app = Flask(__name__)
+
+# check if running in the cloud and set MongoDB settings accordingly
+if 'VCAP_SERVICES' in os.environ:
+    vcap_services = json.loads(os.environ['VCAP_SERVICES'])
+    mongo_credentials = vcap_services['mongodb'][0]['credentials']
+    mongo_uri = mongo_credentials['uri']
+else:
+    mongo_uri = 'mongodb://localhost/db'
+
+app.config['MONGODB_SETTINGS'] = {
+    'host': mongo_uri
+}
+
+# bootstrap our app
+db = MongoEngine(app)
+
+class XmlBlob(db.Document):
+    xml_blob = db.StringField(required=True)
 
 # set the port dynamically with a default of 3000 for local development
 port = int(os.getenv('PORT', '3000'))
@@ -12,6 +34,31 @@ port = int(os.getenv('PORT', '3000'))
 @app.route('/')
 def hello_world():
     return 'Congratulations! Welcome to the Swisscom Application Cloud.'
+
+@app.route('/lora', methods=['POST'])
+def post_xml():
+  # if request.headers['Content-Type'] == 'text/xml':
+    # return request.data
+    xml_new_blob = request.data
+    for xml_obj in XmlBlob.objects:
+        xml_obj.delete()
+    xml_blob_doc = XmlBlob(xml_blob=xml_new_blob)
+    xml_blob_doc.save()
+    return 'XML Blob saved'
+  # else:
+  #   return 'This endpoint requires a text/xml content-type header'
+
+@app.route('/lora', methods=['GET'])
+def get_latest_xml():
+  if not XmlBlob.objects.first() == None:
+    latest_xml_blob = XmlBlob.objects[0].xml_blob
+    xml_obj = untangle.parse(latest_xml_blob)
+    payload_hex = xml_obj.DevEUI_uplink.payload_hex.cdata
+    time = xml_obj.DevEUI_uplink.Time.cdata
+    msg = 'Received payload: {0} with time: {1}'.format(payload_hex, time)
+    return msg
+  else:
+    return 'No data saved'
 
 # start the app
 if __name__ == '__main__':
